83da5d8d62cb14a7b2d8ce74b60690479e185050
Christopher Yuh <christopheryuh9@gmail.com>
2021-03-23 20:17:54 -0400

    there is an error in the Agents.cs decideaction function

--- jumper/Assets/movement.cs
+++ jumper/Assets/movement.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 
-public class movement : MonoBehaviour
+public class movement : Agent
 {
 
     public Rigidbody rb;
@@ -25,14 +23,20 @@ public class movement : MonoBehaviour
         }
     }
 
-    public void CollectObservations(VectorSensor sensor)
-    {
-            sensor.AddObservation(cube.rotation.z);
+    public override void CollectObservations(VectorSensor sensor)
+    {       
+            Vector3 distance = cube.position - target.position;
+
+            sensor.AddObservation(cube.transform.rotation.z);
             sensor.AddObservation(cube.transform.rotation.x);
             sensor.AddObservation(cube.transform.rotation.y);
-            sensor.AddObservation(cube.position - target.position);
-            sensor.AddObservation(rb.velocity);
-    }   
+            sensor.AddObservation(distance.x);
+            sensor.AddObservation(distance.y);
+            sensor.AddObservation(distance.z);
+            sensor.AddObservation(rb.velocity.x);
+            sensor.AddObservation(rb.velocity.y);
+            sensor.AddObservation(rb.velocity.z);
+    }  
     // Start is called before the first frame update
     void Start()
     {
@@ -82,9 +86,6 @@ public class movement : MonoBehaviour
             print(reward);
         }
 
-
-
-
         rb.AddForce(direction);
 
         print("reward:"+reward);
@@ -92,4 +93,69 @@ public class movement : MonoBehaviour
         reward = exaggeration/1000;
         
     }
+
+
+
+
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+        var actionZ = 2f * Mathf.Clamp(actionBuffers.DiscreteActions[0], -1f, 1f);
+        var actionX = 2f * Mathf.Clamp(actionBuffers.DiscreteActions[1], -1f, 1f);
+
+
+        float w = actionBuffers.DiscreteActions[0];
+        float a = actionBuffers.DiscreteActions[1];
+        float s = actionBuffers.DiscreteActions[2];
+        float d = actionBuffers.DiscreteActions[3];
+        float space = actionBuffers.DiscreteActions[4];
+
+        Vector3 direction = new Vector3(0.0f,0.0f,0.0f);
+
+        print(w);
+
+
+        if (space >= .5)
+        {
+            print("space key was pressed");
+            direction.y = force;
+        }
+        
+        if (a >= .5)
+        {   
+            direction.x = -force;
+            print("a key was pressed");
+        }
+        if (d >= .5)
+        {   
+            direction.x = force;
+            print("d key was pressed");
+        }
+
+
+        if (w >= .5)
+        {   
+            direction.z = force;
+            print("w key was pressed");
+        }
+        if (s >= .5)
+        {   
+            direction.z = -force;
+            print("s key was pressed");
+        }
+
+        if (cube.position.y < -10){
+
+            rb.position = new Vector3(0.0f,1.87f,0.0f);
+            reward = -exaggeration;
+            print(reward);
+        }
+
+        rb.AddForce(direction);
+
+        print("reward:"+reward);
+        SetReward(reward);
+        reward = exaggeration/1000;
+    }
+
 }
